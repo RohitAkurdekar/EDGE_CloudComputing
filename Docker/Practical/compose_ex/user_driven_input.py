3c42689f2604d842cfdfbb1480f58cb48998af4b
RohitAkurdekar <rakurdekar@gmail.com>
2023-01-25 08:16:16 +0530

    compose py

--- Docker/Practical/compose_ex/user_driven_input.py
+++ Docker/Practical/compose_ex/user_driven_input.py
@@ -1,8 +1,22 @@
 '''
     This will run as a container
 '''
+
+#------ Add function ------------
 def add_two_number(num1,num2):
     return num1+num2
 
-num1 = int(input("Enter num1: "))
-num2 = int(input("Enter num2: "))
+#------ Main handler --------------
+
+def my_handler():
+    try:
+        #------ Take numbers from user --
+        num1 = int(input("Enter num1: "))
+        num2 = int(input("Enter num2: "))
+        result = add_two_number(num1=num1,num2=num2)
+        print(f"{num1}+{num2}={result}")
+    except Exception as e:
+        print("Exception in main handler: ",e)
+
+# calling main handler
+my_handler()
